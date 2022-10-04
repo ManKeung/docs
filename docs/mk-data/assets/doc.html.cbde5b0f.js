54b56c43bd1eebbbe59ea4820f9ac66cbac81718
mankueng <mankueng1011@gmail.com>
2022-10-04 19:31:16 +0800

    docs: 新增文字效果，修改导航栏文字

---
+++ docs/mk-data/assets/doc.html.cbde5b0f.js
@@ -0,0 +1,21 @@
+import{_ as a,r as l,o as i,c as t,e as c,w as o,b as n,d as e}from"./app.573160db.js";const r={},d=n("h1",{id:"\u8BF4\u660E\u6587\u6863",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u8BF4\u660E\u6587\u6863","aria-hidden":"true"},"#"),e(" \u8BF4\u660E\u6587\u6863")],-1),u=n("div",{class:"custom-container tip"},[n("p",{class:"custom-container-title"},"\u63D0\u793A"),n("p",null,"\u6839\u636E\u56E2\u961F\u89C4\u5B9A")],-1),m=n("div",{class:"language-markdown ext-md line-numbers-mode"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"#"),e(" \u9879\u76EE")]),e(`
+
+\u9879\u76EE\u63CF\u8FF0
+
+`),n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"##"),e(" scripts")]),e(`
+
+\u547D\u4EE4
+
+`),n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"##"),e(" \u6CE8\u610F\u4E8B\u9879")]),e(`
+
+.
+.
+.
+
+`),n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"##"),e(" \u5176\u4ED6\u4FE1\u606F")]),e(`
+
+.
+.
+.
+
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function p(v,b){const s=l("code-type");return i(),t("div",null,[d,u,c(s,{title:"README.md"},{default:o(()=>[m]),_:1})])}const h=a(r,[["render",p],["__file","doc.html.vue"]]);export{h as default};
---
+++ docs/mk-data/assets/icon.html.afd9a84b.js
@@ -0,0 +1,7 @@
+import{_ as t,r as e,o as c,c as i,f as s,e as a,b as o,d as m}from"./app.573160db.js";const r={},h=o("h1",{id:"\u56FE\u6807",tabindex:"-1"},[o("a",{class:"header-anchor",href:"#\u56FE\u6807","aria-hidden":"true"},"#"),m(" \u56FE\u6807")],-1);function l(p,d){const n=e("site");return c(),i("div",null,[s(`
+ * @Description: 
+ * @Author: Mankeung
+ * @Date: 2022-07-11 15:41:51
+ * @LastEditors: Mankeung
+ * @LastEditTime: 2022-07-27 12:55:12
+`),h,a(n,{list:[{name:"iconfont",img:"https://img.alicdn.com/imgextra/i2/O1CN01ZyAlrn1MwaMhqz36G_!!6000000001499-73-tps-64-64.ico",url:"https://www.iconfont.cn/"},{name:"Font Awesome",img:"https://fontawesome.com/images/favicon/icon.svg",url:"http://www.fontawesome.com.cn/"},{name:"Icon Font",img:"https://icomoon.io/favicon.ico",url:"https://icomoon.io/"},{name:"easyicon",img:"https://images.frontendjs.com/easyicon.ico",url:"http://www.easyicon.net/"},{name:"Mono Icons",img:"https://icons.mono.company/favicon.ico",url:"https://icons.mono.company/"},{name:"Boxicons",img:"https://boxicons.com/static/favicon.ico?v=13",url:"https://boxicons.com/"}]},null,8,["list"])])}const _=t(r,[["render",l],["__file","icon.html.vue"]]);export{_ as default};
