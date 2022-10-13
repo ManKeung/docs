044be22cab169ef65d02e61ac75a1faf81756d3f
mankueng <mankueng1011@gmail.com>
2022-10-13 12:50:24 +0800

    docs: 优化

--- docs/mview-ui/assets/index.html.44419899.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as a,c,b as e,d as n}from"./app.5fbcf7bb.js";const o={},r=e("h1",{id:"\u7EC4\u4EF6",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#\u7EC4\u4EF6","aria-hidden":"true"},"#"),n(" \u7EC4\u4EF6")],-1),s=[r];function d(_,i){return a(),c("div",null,s)}const l=t(o,[["render",d],["__file","index.html.vue"]]);export{l as default};
